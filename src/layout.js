f9b2d43281abbea7a4ed464fef288a59d260e55b
Tanner Hornsby <thornsby@cs.uchicago.edu>
2022-11-05 13:37:27 -0500

    decrease interdepencency of classes by adding Definition Object and definitions array prop in Layout

--- src/layout.js
+++ src/layout.js
@@ -1,6 +1,36 @@
 import { doc } from "prettier";
 import { search } from "./search";
 import "./style.scss";
+import dele from './assets/del.png';
+import flag from './assets/flag.png';
+import accumulation_point from './assets/chapter3/accumulation_point.png';
+import bolzano_weierstrass from './assets/chapter3/bolzano-weierstrass.png';
+import boundary_point from './assets/chapter3/boundary_point.png';
+import boundary from './assets/chapter3/boundary.png';
+import bounded_metric_space from './assets/chapter3/bounded-metric-space.png';
+import cauchy_sequence_metric_space from './assets/chapter3/cauchy_sequence-metric-space.png';
+import closed_convex_hull from './assets/chapter3/closed_convex_hull.png';
+import closed_set_metric_space from './assets/chapter3/closed_set-metric-space.png';
+import closure from './assets/chapter3/closure.png';
+import complete_metric_space from './assets/chapter3/complete_metric_space.png';
+import complete_metric_spaces_theorem from './assets/chapter3/complete_metric_spaces-theorem.png';
+import continuous_at_point_x from './assets/chapter3/continuous_at_point_x.png';
+import continuous_functions_metric_spaces from './assets/chapter3/continuous_functions_metric_spaces.png';
+import converge_uniformly from './assets/chapter3/converge_uniformly.png';
+import convex_hull from './assets/chapter3/convex_hull.png';
+import convex from './assets/chapter3/convex.png';
+import diameter from './assets/chapter3/diameter.png';
+import holders_inequality from './assets/chapter3/holders_inequality.png';
+import interior from './assets/chapter3/interior.png';
+import isolated_point from './assets/chapter3/isolated_point.png';
+import limit from './assets/chapter3/limit.png';
+import metric_space_notation1 from './assets/chapter3/metric_space-notation1.png';
+import metric_space from './assets/chapter3/metric_space.png';
+import open_ball_metric_space from './assets/chapter3/open_ball-metric-space.png';
+import open_set_metric_space from './assets/chapter3/open_set-metric-space.png';
+import pointwise_limit from './assets/chapter3/pointwise_limit.png';
+import unit_ball from './assets/chapter3/unit_ball.png';
+import { Element } from './layout'
 
 // make dom element
 export class Element {
@@ -24,19 +54,84 @@ export class Element {
     }
 }
 
+export class Definition extends Element {
+    
+    constructor(def_img, id) {
+        const fla = document.createElement('img');
+        fla.src = flag;
+        fla.title = 'Flag This Definition';
+        fla.classList.add('flag')
+        fla.id = id;
+
+        const def = document.createElement('img');
+        def.src = def_img;
+
+        const del = document.createElement('img');
+        del.src = dele;
+        del.title = 'Remove This Definition';
+        del.classList.add('delete');
+        del.id = id;
+
+        this.type = 'div';
+        this.c = 'definition';
+        this.children = [fla, def, del];
+        this.id = id;
+    }
+}
+
 export class Layout {
     // add content setter
-    constructor(content) {
-        this.content = content;
+    constructor(definitions = []) {
+        child_nodes = [];
+        for(let i = 0; i < definitions.length; i++){
+            const def = new Definition(definitions[i], i);
+            child_nodes.push(def);
+        }
+        this.content = new Element('div', 'definitions', child_nodes);
+        this.definitions = definitions;
     }
 
-    setContent(content) {
-        this.content = content;
+
+    setContent(definitions = []) {
+        child_nodes = [];
+        for(let i = 0; i < definitions.length; i++){
+            const def = new Definition(definitions[i], i);
+            child_nodes.push(def.getDOM());
+        }
+        this.content = new Element('div', 'definitions', child_nodes);
+        this.definitions = definitions;
     }
+
 
     resetDOM() {
         document.body.textContent = '';
     }
+
+
+    setDOMEvents(){
+        // add flagging event
+        const flags = document.querySelectorAll('flag');
+        flags.forEach((flag) => {
+            flag.addEventListener('click', ()=>{
+                this.definitions.unshift(this.definitions[flag.id]);
+                this.resetDOM();
+                this.setDOM();
+                this.setDOMEvents();
+            });
+        });
+
+        // add deletion event
+        const dels = document.querySelectorAll('flag');
+        dels.forEach((del) => {
+            del.addEventListener('click', ()=>{
+                this.definitions[del.id].remove();
+                this.resetDOM();
+                this.setDOM();
+                this.setEvents();
+            });
+        });       
+    }
+
 
     setDOM() {
         // setting body
@@ -53,20 +148,21 @@ export class Layout {
             window.location.href = "https://github.com/TannerHornsby7/Analysistant";
         };
         const cta = document.createElement('h3');
-        const search_bar = document.createElement('input');
-        const search_btn = document.createElement('button');
-        cta.textContent = 'Enter Your Question Below, We will search for the relevant definitions that can help with your proof!';
+        cta.textContent = 'Enter Your Question Below, We will search for the ' +
+        'relevant definitions that can help with your proof!';
         search_btn.id = 'search_btn';
 
+        // search set up
+        const search_bar = document.createElement('input');
+        const search_btn = document.createElement('button');
+
+        // adding search event listener
         function commence_search() {
-            console.log('clicked');
-            
-            const definitions = search(search_bar.value);
-            const definition_sect = new Element('div', 'definitions', definitions);
-            
-            const update = new Layout(definition_sect.getDOM());
-            update.resetDOM();
-            update.setDOM();
+            const def_urls = search(search_bar.value);
+
+            for(const i in def_urls) {
+                
+            }
             const question = document.getElementById('question');
             question.textContent = 'Question: ' + search_bar.value;
         };
--- src/search.js
+++ src/search.js
@@ -1,34 +1,4 @@
 import "./style.scss";
-import del from './assets/del.png';
-import flag from './assets/flag.png';
-import accumulation_point from './assets/chapter3/accumulation_point.png';
-import bolzano_weierstrass from './assets/chapter3/bolzano-weierstrass.png';
-import boundary_point from './assets/chapter3/boundary_point.png';
-import boundary from './assets/chapter3/boundary.png';
-import bounded_metric_space from './assets/chapter3/bounded-metric-space.png';
-import cauchy_sequence_metric_space from './assets/chapter3/cauchy_sequence-metric-space.png';
-import closed_convex_hull from './assets/chapter3/closed_convex_hull.png';
-import closed_set_metric_space from './assets/chapter3/closed_set-metric-space.png';
-import closure from './assets/chapter3/closure.png';
-import complete_metric_space from './assets/chapter3/complete_metric_space.png';
-import complete_metric_spaces_theorem from './assets/chapter3/complete_metric_spaces-theorem.png';
-import continuous_at_point_x from './assets/chapter3/continuous_at_point_x.png';
-import continuous_functions_metric_spaces from './assets/chapter3/continuous_functions_metric_spaces.png';
-import converge_uniformly from './assets/chapter3/converge_uniformly.png';
-import convex_hull from './assets/chapter3/convex_hull.png';
-import convex from './assets/chapter3/convex.png';
-import diameter from './assets/chapter3/diameter.png';
-import holders_inequality from './assets/chapter3/holders_inequality.png';
-import interior from './assets/chapter3/interior.png';
-import isolated_point from './assets/chapter3/isolated_point.png';
-import limit from './assets/chapter3/limit.png';
-import metric_space_notation1 from './assets/chapter3/metric_space-notation1.png';
-import metric_space from './assets/chapter3/metric_space.png';
-import open_ball_metric_space from './assets/chapter3/open_ball-metric-space.png';
-import open_set_metric_space from './assets/chapter3/open_set-metric-space.png';
-import pointwise_limit from './assets/chapter3/pointwise_limit.png';
-import unit_ball from './assets/chapter3/unit_ball.png';
-import { Element } from './layout'
 
 // term collection
 const terms = [
@@ -68,8 +38,8 @@ const terms = [
     'unit'
 ];
 
-// keys to urls
-const k_urls = [
+// url aliases
+const a_urls = [
     accumulation_point,
     bolzano_weierstrass,
     boundary_point,
@@ -141,9 +111,8 @@ for (const term in terms) {
 // hash map array populator
 for (const term in terms) {
     for (const s in s_urls){
-        if (s_urls[s].includes(terms[term]) && terms_hash[terms[term]].includes(k_urls[s]) == 0) {
-            terms_hash[terms[term]].push(k_urls[s]);
-            // console.log(s_urls[s]);
+        if (s_urls[s].includes(terms[term]) && terms_hash[terms[term]].includes(a_urls[s]) == 0) {
+            terms_hash[terms[term]].push(a_urls[s]);
         }
     }
 }
@@ -158,39 +127,12 @@ export function search(words) {
     let used_terms = []
     for (var word in words) {
         for(const url in terms_hash[words[word]]) {
-            const def = document.createElement('img');
             const d_key = terms_hash[words[word]][url];
-            def.classList.add('definition');
             if (used_terms.includes(d_key) == false) {
                 // collect reused definitions
-                used_terms.push(d_key);
-                def.src = d_key;
-                def.id = d_key; // change id's to be
-
-                // add deletion functionality
-                const dele = document.createElement('img');
-                dele.src = del;
-                dele.title = 'Remove This Definition';
-                dele.classList.add('delete');
-                dele.id = d_key;
-                dele.addEventListener('click', ()=>{
-                    const def_to_del = document.getElementById(d_key);
-                    console.log(def_to_del)
-                    def_to_del.remove();
-                });
-                
-                // add flagging functionality
-                const fla = document.createElement('img');
-                fla.src = flag;
-                fla.title = 'Flag This Definition';
-                fla.classList.add('flag')
-                fla.id = d_key;
-                const def_sect = new Element('div', 'def_sect', [fla, def, dele]);
-                const def_section = def_sect.getDOM();
-                def_section.id = d_key;
-            
+                used_terms.push(d_key);            
                 // add node to definitions node list
-                definitions.push(def_section);
+                definitions.push(d_key);
             }
         }
     }
